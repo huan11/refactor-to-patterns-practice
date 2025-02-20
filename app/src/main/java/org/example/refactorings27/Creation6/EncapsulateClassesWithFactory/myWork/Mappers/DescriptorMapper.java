2ee2e53ba42a8d7709291f23c683646b4d2c930d
asher <springflybird@163.com>
2025-02-21 07:04:24 +0800

    go to DefaultDescriptor class and use `Use Interface Where Possible`

--- app/src/main/java/org/example/refactorings27/Creation6/EncapsulateClassesWithFactory/myWork/Mappers/DescriptorMapper.java
+++ app/src/main/java/org/example/refactorings27/Creation6/EncapsulateClassesWithFactory/myWork/Mappers/DescriptorMapper.java
@@ -31,8 +31,9 @@ public class DescriptorMapper {
     /**
      * 1 不需要的参数可以内联 inline parameter
      * 2 方法返回值
+     * 3 go to DefaultDescriptor class and use `Use Interface Where Possible`
      */
-    private static DefaultDescriptor forInteger(String remoteId, Class<?> descriptorClass) {
+    private static AttributeDescriptor forInteger(String remoteId, Class<?> descriptorClass) {
         return new DefaultDescriptor(remoteId, descriptorClass, Integer.class);
     }
 
