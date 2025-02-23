113682acef1960f3f8e3b5f5d37082bde782f556
asher <springflybird@163.com>
2025-02-23 14:41:51 +0800

    Test 1 testBuildOneNode

--- app/src/main/java/org/example/refactorings27/Creation6/EncapsulateCompositeWithBuilder/myWork/TagBuilder.java
+++ app/src/main/java/org/example/refactorings27/Creation6/EncapsulateCompositeWithBuilder/myWork/TagBuilder.java
@@ -1,4 +1,13 @@
 package org.example.refactorings27.Creation6.EncapsulateCompositeWithBuilder.myWork;
 
 public class TagBuilder {
+    private TagNode rootNode;
+
+    public TagBuilder(String rootTagName) {
+        rootNode = new TagNode(rootTagName);
+    }
+
+    public String toXml() {
+        return rootNode.toString();
+    }
 }
