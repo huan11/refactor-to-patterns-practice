1e802692bd977732e797d5a15b544648fc3f7dcc
asher <springflybird@163.com>
2025-02-24 07:49:24 +0800

    [ReplaceConditionalLogicWithStrategy][Move method]: Step1: declare capital() in CapitalStrategy

--- app/src/main/java/org/example/refactorings27/Simplification6/ReplaceConditionalLogicWithStrategy/myWork/CapitalStrategy.java
+++ app/src/main/java/org/example/refactorings27/Simplification6/ReplaceConditionalLogicWithStrategy/myWork/CapitalStrategy.java
@@ -1,4 +1,7 @@
 package org.example.refactorings27.Simplification6.ReplaceConditionalLogicWithStrategy.myWork;
 
 public class CapitalStrategy {
+    public double capital() {
+        return 0.0;
+    }
 }
